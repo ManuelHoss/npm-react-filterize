0b081b360ee60f9d4223a31687b867331f73f586
Manuel Hoss <manuel.hoss@konzepthaus-ws.de>
2020-01-29 18:52:10 +0100

    initial commit

---
+++ examples/all-filter-types/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Filterize from '../../..';
+
+function App() {
+    return (
+        <div className="App">
+            <Filterize />
+        </div>
+    );
+}
+
+export default App;
